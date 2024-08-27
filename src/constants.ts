bf53e052c4724328c2d785ac466071834db3532e
cstef <colin@petit-suisse.fr>
2024-08-27 23:06:24 +0200

    fix: theme if a non existent theme was already present in localStorage

---
+++ src/constants.ts
@@ -0,0 +1,34 @@
+export const THEMES = [
+	{
+		name: "Atom One Dark",
+		id: "atomOneDark",
+	},
+	{
+		name: "Catppuccin",
+		id: "catppuccin",
+	},
+	{
+		name: "Dracula",
+		id: "dracula",
+	},
+	{
+		name: "Gruvbox",
+		id: "gruvbox",
+	},
+	{
+		name: "Monokai",
+		id: "monokai",
+	},
+	{
+		name: "Nord",
+		id: "nord",
+	},
+	{
+		name: "Solarized",
+		id: "solarized",
+	},
+	{
+		name: "Github",
+		id: "default",
+	},
+];
