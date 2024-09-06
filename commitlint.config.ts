5934b4b783bf23340d588db33e155ff289f44bba
cstef <colin@petit-suisse.fr>
2024-09-06 18:00:52 +0200

    content: update posts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,4 +2,22 @@ import type { UserConfig } from "@commitlint/types";
 
 export default {
 	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat",
+				"fix",
+				"docs",
+				"style",
+				"refactor",
+				"perf",
+				"test",
+				"chore",
+				"revert",
+				"content",
+			],
+		],
+	},
 } satisfies UserConfig;
