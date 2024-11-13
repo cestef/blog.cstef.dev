632c2210811d2a05dff4cf3a1bc2193f6d843dc9
cstef <root@cstef.dev>
2024-11-13 10:03:51 +0100

    fix: remove compiled content from rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,9 +2,8 @@ import { HIDE_SEEDLINGS_PROD } from "@/constants";
 import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
-import { experimental_AstroContainer as AstroContainer } from "astro/container";
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const GET: APIRoute = async () => {
 	const blogEntries = await getCollection(
 		"blog",
 		(entry) =>
@@ -14,20 +13,15 @@ export const GET: APIRoute = async ({ params, request }) => {
 				entry.data.growth === "seedling"
 			),
 	);
-	const container = await AstroContainer.create();
 	const posts = await Promise.all(
 		blogEntries.map(async (entry) => {
 			return {
 				...entry.data,
-				compiledContent: await container.renderToString(
-					(await entry.render()).Content,
-				),
 				slug: entry.slug,
 			};
 		}),
 	).then((posts) => posts.sort((a, b) => b.date.getTime() - a.date.getTime()));
-	const encodeHtml = (str: string) =>
-		str.replace(/[^\u0009\u000A\u000D\u0020-\uD7FF\uE000-\uFFFD]+/gu, "");
+
 	return rss({
 		title: "cstef's Digital Garden",
 		description:
@@ -36,7 +30,6 @@ export const GET: APIRoute = async ({ params, request }) => {
 			link: `https://blog.cstef.dev/post/${post.slug}`,
 			pubDate: post.date,
 			categories: post.tags,
-			content: encodeHtml(post.compiledContent),
 			...post,
 		})),
 		site: "https://blog.cstef.dev",
