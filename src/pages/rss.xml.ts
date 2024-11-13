7faa18cfd77e5a0475ee6b96cb959e5af9130186
cstef <root@cstef.dev>
2024-11-13 09:16:20 +0100

    feat: add rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,40 @@
+import { HIDE_SEEDLINGS_PROD } from "@/constants";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async ({ params, request }) => {
+	const blogEntries = await getCollection(
+		"blog",
+		(entry) =>
+			!(
+				import.meta.env.PROD &&
+				HIDE_SEEDLINGS_PROD &&
+				entry.data.growth === "seedling"
+			),
+	);
+	const posts = await Promise.all(
+		blogEntries.map(async (entry) => {
+			const rendered = await entry.render();
+			return {
+				...entry.data,
+				compiledContent: rendered,
+				slug: entry.slug,
+			};
+		}),
+	).then((posts) => posts.sort((a, b) => b.date.getTime() - a.date.getTime()));
+
+	return rss({
+		title: "cstef's Digital Garden",
+		description:
+			"A blog about software development, maths, or whatever else I feel like writing about.",
+		items: posts.map((post) => ({
+			link: `https://blog.cstef.dev/post/${post.slug}`,
+			pubDate: post.date,
+			categories: post.tags,
+			...post,
+		})),
+		site: "https://blog.cstef.dev",
+		stylesheet: "/styles.xsl",
+	});
+};
