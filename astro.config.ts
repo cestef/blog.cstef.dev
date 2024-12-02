e4550204ad1566f9591f75687a732910dc3776a2
cstef <colin@petit-suisse.fr>
2024-12-02 17:02:30 +0100

    chore: add minification

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import rehypeCallouts from "rehype-callouts";
 import rehypeSlug from "rehype-slug";
 import rehypePrettyCode, {
-	type Options as PrettyCodeOptions,
+    type Options as PrettyCodeOptions,
 } from "rehype-pretty-code";
 import { rehypeTwemoji } from "rehype-twemoji";
 import remarkMath from "remark-math";
@@ -13,9 +13,9 @@ import remarkEmoji from "remark-emoji";
 import { rehypePikchr } from "./plugins/pikchr";
 import rehypeTypst from "./plugins/typst";
 import {
-	bundledLanguages,
-	createCssVariablesTheme,
-	getSingletonHighlighter,
+    bundledLanguages,
+    createCssVariablesTheme,
+    getSingletonHighlighter,
 } from "shiki";
 import pikchrLang from "./syntaxes/pikchr.tmLanguage.json";
 import icon from "astro-icon";
@@ -30,64 +30,67 @@ import playformCompress from "@playform/compress";
 import purgecss from "astro-purgecss";
 import { remarkInclude } from "./plugins/include";
 
+import min from "astro-min";
+
 const shikiTheme = createCssVariablesTheme({
-	name: "default",
-	variablePrefix: "--shiki-",
-	variableDefaults: {},
-	fontStyle: true,
+    name: "default",
+    variablePrefix: "--shiki-",
+    variableDefaults: {},
+    fontStyle: true,
 });
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	site: "https://blog.cstef.dev",
-	integrations: [
-		tailwind(),
-		sitemap(),
-		mdx(),
-		icon(),
-		playformCompress(),
-		purgecss(),
-	],
-	build: {
-		inlineStylesheets: "never",
-	},
-	markdown: {
-		rehypePlugins: [
-			rehypeRaw,
-			rehypeCopy,
-			rehypeSlug,
-			rehypeCallouts,
-			rehypePikchr,
-			rehypeTypst,
-			rehypeTypstRaw,
-			[
-				rehypePrettyCode,
-				{
-					getHighlighter: () =>
-						getSingletonHighlighter({
-							themes: [shikiTheme],
-							langs: [...Object.values(bundledLanguages), pikchrLang] as any,
-						}),
-					// @ts-ignore
-					theme: "default",
-				} satisfies PrettyCodeOptions,
-			],
-			[
-				rehypeTwemoji,
-				{
-					format: "svg",
-					source: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest",
-				},
-			],
-		],
-		remarkPlugins: [
-			remarkInclude,
-			remarkMath,
-			remarkEmoji,
-			remarkReadingTime,
-			remarkModifiedTime,
-		],
-		syntaxHighlight: false,
-	},
+    output: "static",
+    site: "https://blog.cstef.dev",
+    integrations: [
+      tailwind(),
+      sitemap(),
+      mdx(),
+      icon(),
+      playformCompress(),
+      purgecss(),
+      min(),
+    ],
+    build: {
+        inlineStylesheets: "never",
+    },
+    markdown: {
+        rehypePlugins: [
+            rehypeRaw,
+            rehypeCopy,
+            rehypeSlug,
+            rehypeCallouts,
+            rehypePikchr,
+            rehypeTypst,
+            rehypeTypstRaw,
+            [
+                rehypePrettyCode,
+                {
+                    getHighlighter: () =>
+                        getSingletonHighlighter({
+                            themes: [shikiTheme],
+                            langs: [...Object.values(bundledLanguages), pikchrLang] as any,
+                        }),
+                    // @ts-ignore
+                    theme: "default",
+                } satisfies PrettyCodeOptions,
+            ],
+            [
+                rehypeTwemoji,
+                {
+                    format: "svg",
+                    source: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest",
+                },
+            ],
+        ],
+        remarkPlugins: [
+            remarkInclude,
+            remarkMath,
+            remarkEmoji,
+            remarkReadingTime,
+            remarkModifiedTime,
+        ],
+        syntaxHighlight: false,
+    },
 });
