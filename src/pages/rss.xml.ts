6ecedb5590b0edbc96d9ed7df8cdf9936eb64f91
cstef <root@cstef.dev>
2024-12-02 07:57:24 +0100

    refactor: move paths to plural

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -27,7 +27,7 @@ export const GET: APIRoute = async () => {
 		description:
 			"A blog about software development, maths, or whatever else I feel like writing about.",
 		items: posts.map((post) => ({
-			link: `https://blog.cstef.dev/post/${post.slug}`,
+			link: `https://blog.cstef.dev/posts/${post.slug}`,
 			pubDate: post.date,
 			categories: post.tags,
 			...post,
